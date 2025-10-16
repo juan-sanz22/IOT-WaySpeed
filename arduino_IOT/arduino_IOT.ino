75202f215f063ff7f318d5a4f0907f80860ca5d5
juan_sanz <juan_sanz@estudante.sesisenai.org.br>
2025-10-16 10:13:29 -0300

    Fiz a part que foi pedida em aula

--- arduino_IOT/arduino_IOT.ino
+++ arduino_IOT/arduino_IOT.ino
@@ -1,18 +1,18 @@
 #include <WiFi.h>
 
-const String SSID ="FIESC_IOT_EDU";
-const String PASS ="8120gv08";
-
+const String SSID = "nome";
+const String Pass = "senha";
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("conectando ao WiFi");
+  Serial.prntln("Conectado ao Wifi");
   WiFi.begin(SSID,PASS);
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(200);
   }
-  Serial.println("/nconectado com Sucesso!");
+  Serial.prntkn("\nConectando com sucesso");
+
 }
 
 void loop() {
