6c0561c4847047fe9a151546f9a713400b7962f9
juan_sanz <juan_sanz@estudante.sesisenai.org.br>
2025-10-30 09:59:02 -0300

    Trocado credenciais por info no env.h

--- Trem/Trem.ino
+++ Trem/Trem.ino
@@ -1,21 +1,16 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <env.h>
 
 WiFiClient client;
 PubSubClient mqtt(client);
 
-const String BrokerURL = "test.mosquitto.org"; //endereço do broker público
-const int BrokerPort = 1883;                   //porta do broker público
-const String BrokerUser = "";  //usuário do servidor
-const String BrokerPass = "";  //senha do servidor
 
-const String SSID = "FIESC_IOT_EDU";  //Nome do WiFi
-const String PASS = "8120gv08";  //Senha do WiFi
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Conectando ao WiFi"); //apresenta a mensagem na tela
-  WiFi.begin(SSID,PASS); //tenta conectar na rede
+  WiFi.begin(WIFI_SSID,WIFI_PASS); //tenta conectar na rede
   while(WiFi.status() != WL_CONNECTED){
     Serial.print("."); // mostra "....."
     delay(200);
@@ -23,22 +18,22 @@ void setup() {
   Serial.println("\nConectado com Sucesso!");
 
   Serial.println("Conectando ao Broker...");
-  mqtt.setServer(BrokerURL.c_str(),BrokerPort);
+  mqtt.setServer(BROKER_URL,BrokerPort);
   String BoardID = "s2";
   BoardID += String(random(0xffff),HEX);
-  mqtt.connect(BoardID.c_str() , BrokerUser.c_str() , BrokerPass.c_str());
+  mqtt.connect(BoardID.c_str() , Broker_User, Broker_Pass);
   while(!mqtt.connected()){
     Serial.print(".");
     delay(200);
   }
-  mqtt.subscribe("Topico-DSM14")
+  mqtt.subscribe(TOPIC_ILUM)
   Serial.println("\nConectado ao Broker!");
 }
 
 void loop() {
 String mensagem = "Nome:Juan";
 mensagem+= "oi";
-mqtt.publish("Topico-DSM14" , mensagem.c_str());
+mqtt.publish(TOPIC_ILUM , mensagem.c_str());
 mqtt.loop();
 delay(1000);
 }
@@ -49,9 +44,9 @@ void callback(char* topic, byte* payload, usigned int length){
   for(int i = 0; i < length; i++){
     msg += (char) payload[i]
   }
-  if(topic == "iluminacao" && msg == "Acender"){
+  if(topic == "TOPIC_ILUM" && msg == "Acender"){
     digitalWrite(2,HIGH);
-    if(topic == "iluminacao" && msg == "Apagar"){
+    if(topic == "TOPIC_ILUM" && msg == "Apagar"){
     digitalWrite(2,LOW);
   }
 }
