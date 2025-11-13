1cc8c281fd63a60476e9d483694fc7b3d4e80853
juan_sanz <you@example.com>
2025-11-13 08:58:01 -0300

    Fiz a mensagem aparecer no Hive MQ

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,55 +1,86 @@
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
-#include <env.h>
+#include "env.h"
 
 WiFiClientSecure client;
 PubSubClient mqtt(client);
 
-void setup() {
-  Serial.begin(115200);
-  client.setInsecure();
-  Serial.println("Conectando ao WiFi"); //apresenta a mensagem na tela
-  WiFi.begin(WIFI_SSID,WIFI_PASS); //tenta conectar na rede
-  while(WiFi.status() != WL_CONNECTED){
-    Serial.print("."); // mostra "....."
-    delay(200);
+// Função de callback (chamada quando uma mensagem é recebida)
+void callback(char* topic, byte* payload, unsigned int length) {
+  String msg = "";
+  
+  for (int i = 0; i < length; i++) {
+    msg += (char)payload[i];
   }
-  Serial.println("\nConectado com Sucesso!");
 
-  Serial.println("Conectando ao Broker...");
-  mqtt.setServer(BROKER_URL, BROKER_PORT);
-  String BoardID = "s2";
-  while(!mqtt.connected()){
-    BoardID += String(random(0xffff),HEX);
-    mqtt.connect(BoardID.c_str() , BROKER_USER, BROKER_PORT;
-    Serial.print(".");
-    delay(200);
+  Serial.print("Mensagem recebida no tópico: ");
+  Serial.println(topic);
+  Serial.print("Conteúdo: ");
+  Serial.println(msg);
+
+  if (String(topic) == TOPIC_ILUM) {
+    if (msg == "Acender") {
+      digitalWrite(2, HIGH);
+      Serial.println("Luz ligada!");
+    } 
+    else if (msg == "Apagar") {
+      digitalWrite(2, LOW);
+      Serial.println("Luz desligada!");
+    }
   }
-  Serial.println("\nConectado ao Broker!");
-  mqtt.subscribe(TOPIC_ILUM);
+}
+
+void reconnect() {
+  while (!mqtt.connected()) {
+    Serial.print("Tentando conectar ao broker MQTT...");
+    String BoardID = "S2_" + String(random(0xffff), HEX);
+    
+    // Tenta conectar (usuário e senha vazios estão OK)
+    if (mqtt.connect(BoardID.c_str(), BROKER_USER, BROKER_PASS)) {
+      Serial.println("Conectado!");
+      mqtt.subscribe(TOPIC_ILUM);
+    } else {
+      Serial.print("Falha, rc=");
+      Serial.print(mqtt.state());
+      Serial.println(" tentando novamente em 2 segundos...");
+      delay(2000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+
+  client.setInsecure();  // SSL sem verificação de certificado
+  
+  Serial.println("Conectando ao WiFi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi conectado!");
+  Serial.print("IP local: ");
+  Serial.println(WiFi.localIP());
+
+  mqtt.setServer(BROKER_URL, BROKER_PORT);
+  mqtt.setCallback(callback);
+  
+  reconnect(); // Conecta ao broker
 }
 
 void loop() {
-String mensagem = "Nome:Juan";
-mensagem+= "oi";
-mqtt.publish(TOPIC_ILUM , mensagem.c_str());
-mqtt.loop();
-delay(1000);
-}
-
-
-void callback(char* topic, byte* payload, usigned int length){
-  string msg = "";
-  for(int i = 0; i < length; i++){
-    msg += (char) payload[i]
+  if (!mqtt.connected()) {
+    reconnect();
   }
-  if(topic == TOPIC_ILUM && msg == "Acender"){
-    digitalWrite(2,HIGH);
-    if(topic == TOPIC_ILUM && msg == "Apagar"){
-  if(topic == TOPIC_ILUM && msg == "Acender"){
-    digitalWrite(2,HIGH);
-    if(topic == TOPIC_ILUM && msg == "Apagar"){
-    digitalWrite(2,LOW);
-  }
+
+  String mensagem = "Nome: Juan - Salave salve quebrada";
+  mqtt.publish(TOPIC_ILUM, mensagem.c_str());
+
+  mqtt.loop();
+  delay(2000);
 }
